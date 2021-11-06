c8bac38cce56a49c6f7d8cc5bdcf93a1a01ed890
Dmitry Buravkin <dmitry.buravkin@gmail.com>
2021-11-06 09:22:04 +0300

    feat: completed 04-cody-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const fs = require('fs');
+const path = require('path');
 
+async function copyDir() {
+  const originalDirPath = path.join(__dirname, 'files');
+  const copyDirPath = path.join(__dirname, 'files-copy');
+
+  await fs.promises.mkdir(copyDirPath, { recursive: true });
+
+  const oldFiles = await fs.promises.readdir(copyDirPath);
+
+  if (oldFiles.length) {
+    for (let file of oldFiles) {
+      await fs.promises.unlink(path.join(copyDirPath, file));
+    }
+  }
+
+  const originalFiles = await fs.promises.readdir(originalDirPath);
+  try {
+    for (let file of originalFiles) {
+      const originalFilePath = path.join(originalDirPath, file);
+      const copyFilePath = path.join(copyDirPath, file);
+      
+      await fs.promises.copyFile(originalFilePath, copyFilePath);
+    }
+  } catch {
+    console.error(error);
+  }
+
+  console.log('Сopying completed');
+}
+
+copyDir();
